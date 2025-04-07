6716738efe1d6164166112ebaea0a267bdffc8e8
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-04-07 23:07:16 +0200

    grab favicon on vercel

--- app/layout.js
+++ app/layout.js
@@ -12,34 +12,9 @@ export const metadata = {
   title: "CalEnder - A Calendar App for Endermen",
   description: "The first calendar app designed specifically for Endermen, featuring a soothing dark interface that's easy on Enderman eyes.",
   icons: {
-    icon: [
-      {
-        url: '/favicon.ico',
-        sizes: 'any',
-      },
-      {
-        url: '/icon.png',
-        type: 'image/png',
-        sizes: '32x32',
-      },
-      {
-        url: '/icon-192.png',
-        type: 'image/png',
-        sizes: '192x192',
-      },
-      {
-        url: '/icon-512.png',
-        type: 'image/png',
-        sizes: '512x512',
-      },
-    ],
-    apple: [
-      {
-        url: '/apple-icon.png',
-        type: 'image/png',
-        sizes: '180x180',
-      },
-    ],
+    icon: '/icon.png',
+    shortcut: '/icon.png',
+    apple: '/apple-icon.png',
   },
 };
 
